edcab55104d70b4353579397e605d64400e963a6
Hugo <54435874+Cactooz@users.noreply.github.com>
2023-01-22 10:59:36 +0100

    Add base GUI project files

---
+++ BrainfuckConverterGUI/ConverterGUI.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace BrainfuckConverterGUI {
+    partial class ConverterGUI {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.inputText = new System.Windows.Forms.RichTextBox();
+            this.encodeButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.outputText = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // inputText
+            // 
+            this.inputText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.inputText.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputText.Location = new System.Drawing.Point(100, 125);
+            this.inputText.Name = "inputText";
+            this.inputText.ShowSelectionMargin = true;
+            this.inputText.Size = new System.Drawing.Size(400, 100);
+            this.inputText.TabIndex = 0;
+            this.inputText.Text = "";
+            // 
+            // encodeButton
+            // 
+            this.encodeButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.encodeButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.encodeButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.encodeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.encodeButton.Location = new System.Drawing.Point(250, 250);
+            this.encodeButton.Name = "encodeButton";
+            this.encodeButton.Padding = new System.Windows.Forms.Padding(5);
+            this.encodeButton.Size = new System.Drawing.Size(100, 50);
+            this.encodeButton.TabIndex = 1;
+            this.encodeButton.Text = "Encode";
+            this.encodeButton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(96, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Extended ASCII Input";
+            // 
+            // outputText
+            // 
+            this.outputText.Cursor = System.Windows.Forms.Cursors.Default;
+            this.outputText.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputText.Location = new System.Drawing.Point(100, 380);
+            this.outputText.Name = "outputText";
+            this.outputText.ReadOnly = true;
+            this.outputText.Size = new System.Drawing.Size(400, 100);
+            this.outputText.TabIndex = 3;
+            this.outputText.Text = "";
+            // 
+            // ConverterGUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(584, 611);
+            this.Controls.Add(this.outputText);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.encodeButton);
+            this.Controls.Add(this.inputText);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "ConverterGUI";
+            this.Text = "BRAINFUCK Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox inputText;
+        private System.Windows.Forms.Button encodeButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox outputText;
+    }
+}
+
