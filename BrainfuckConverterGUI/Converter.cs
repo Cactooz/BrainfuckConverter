81a35b1febc5110cfd98f79b066b7548c32a4db3
Hugo <54435874+Cactooz@users.noreply.github.com>
2023-01-22 12:33:09 +0100

    Move all Converter logic into the Converter

--- BrainfuckConverterGUI/Converter.cs
+++ BrainfuckConverterGUI/Converter.cs
@@ -1,69 +1,101 @@
 ﻿namespace BrainfuckConverterGUI {
 	internal class Converter {
-        /// <summary>
-        /// The BRAINFUCK working memory cells with their current values.
-        /// </summary>
-        private readonly int[] memory;
-        /// <summary>
-        /// Where BRAINFUCK currently points at in the working memory.
-        /// </summary>
-        private int pointer;
-        /// <summary>
-        /// BRAINFUCK code executed first to set up the memory cells.
-        /// </summary>
-        private readonly string setup = "";
-        /// <summary>
-        /// The optional <see cref="setup"/> code for BRAINFUCK working <see cref="memory">memory</see>.
-        /// </summary>
-        public string Setup { get => setup; }
+		/// <summary>
+		/// The BRAINFUCK working memory cells with their current values.
+		/// </summary>
+		private readonly int[] memory;
+		/// <summary>
+		/// Where BRAINFUCK currently points at in the working memory.
+		/// </summary>
+		private int pointer;
+		/// <summary>
+		/// BRAINFUCK code executed first to set up the memory cells.
+		/// </summary>
+		private readonly string setup = "";
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Converter"/> class with custom <see cref="memory"/> setup.
+		/// </summary>
+		/// <param name="memory">The BRAINFUCK working memory cells and their current values</param>
+		/// <param name="pointer">Where BRAINFUCK currently points at in the working memory.</param>
+		public Converter(int[] memory, int pointer) {
+			this.memory = memory;
+			this.pointer = pointer;
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Converter"/> class with default <see cref="memory"/> setup.
+		/// </summary>
+		public Converter() {
+			setup = "++++++++++[>+++++++++>++++++++++++>+++++++++++++>++++++>+++++>++++++++>+++++++++++>++++>++++++++++++++++++++++<<<<<<<<<-]>+>+++>>>>";
+			memory = new int[] { 0, 91, 123, 130, 60, 50, 80, 110, 40, 220 };
+			pointer = 6;
+		}
+
+		/// <summary>
+		/// Encode the <paramref name="inputText"/> <see cref="string"/> of Extended ASCII characters into BRAINFUCK code.
+		/// </summary>
+		/// <param name="inputText">The text <see cref="string"/> of Extended ASCII to encode.</param>
+		/// <returns></returns>
+		public string Encode(string inputText) {
+			string output = "";
+
+			foreach(char inputChar in inputText) {
+				if(inputChar >= 65 && inputChar <= 90)
+					output += GetCode(inputChar, 6);
+				else if(inputChar >= 97 && inputChar <= 122)
+					output += GetCode(inputChar, 7);
+				else if(inputChar >= 32 && inputChar <= 48)
+					output += GetCode(inputChar, 8);
+				else if(inputChar >= 48 && inputChar <= 57)
+					output += GetCode(inputChar, 5);
+				else if(inputChar >= 58 && inputChar <= 64)
+					output += GetCode(inputChar, 4);
+				else if(inputChar >= 192 && inputChar <= 255)
+					output += GetCode(inputChar, 9);
+				else if(inputChar >= 128 && inputChar <= 191)
+					output += GetCode(inputChar, 3);
+				else if(inputChar >= 123 && inputChar <= 126)
+					output += GetCode(inputChar, 2);
+				else if(inputChar >= 91 && inputChar <= 96)
+					output += GetCode(inputChar, 1);
+				else if(inputChar < 0 || inputChar > 255)
+					continue;
+				else
+					output += GetCode(inputChar, 0);
+			}
+
+			return setup + output;
+		}
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="Converter"/> class with custom <see cref="memory"/> setup.
+        /// Gets a <see cref="string"/> of BRAINFUCK code to print the <paramref name="input"/> character.
         /// </summary>
-        /// <param name="memory">The BRAINFUCK working memory cells and their current values</param>
-        /// <param name="pointer">Where BRAINFUCK currently points at in the working memory.</param>
-        public Converter(int[] memory, int pointer) {
-            this.memory = memory;
-            this.pointer = pointer;
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Converter"/> class with default <see cref="memory"/> setup.
-        /// </summary>
-        public Converter() {
-            setup = "++++++++++[>+++++++++>++++++++++++>+++++++++++++>++++++>+++++>++++++++>+++++++++++>++++>++++++++++++++++++++++<<<<<<<<<-]>+>+++>>>>";
-            memory = new int[] { 0, 91, 123, 130, 60, 50, 80, 110, 40, 220 };
-            pointer = 6;
-        }
-
-        /// <summary>
-        /// Encode a <see cref="string"/> of Extended ASCII characters into BRAINFUCK code.
-        /// </summary>
-        /// <param name="input">The <see cref="char"/> get movements to and print.</param>
+        /// <param name="input">The <see cref="char"/> to get movements for and print.</param>
         /// <param name="cell">The cell in the memory where the <paramref name="input"/> character exists.</param>
         /// <returns>A <see cref="string"/> of movements for the BRAINFUCK code.</returns>
-        public string Encode(char input, int cell) {
-            string output = "";
+        public string GetCode(char input, int cell) {
+			string output = "";
 
-            while(pointer < cell) {
-                output += ">";
-                pointer++;
-            }
-            while(pointer > cell) {
-                output += "<";
-                pointer--;
-            }
-            while(memory[pointer] < input) {
-                output += "+";
-                memory[pointer]++;
-            }
-            while(memory[pointer] > input) {
-                output += "-";
-                memory[pointer]--;
-            }
-            output += ".";
+			while(pointer < cell) {
+				output += ">";
+				pointer++;
+			}
+			while(pointer > cell) {
+				output += "<";
+				pointer--;
+			}
+			while(memory[pointer] < input) {
+				output += "+";
+				memory[pointer]++;
+			}
+			while(memory[pointer] > input) {
+				output += "-";
+				memory[pointer]--;
+			}
+			output += ".";
 
-            return output;
-        }
-    }
+			return output;
+		}
+	}
 }
--- BrainfuckConverterGUI/ConverterGUI.cs
+++ BrainfuckConverterGUI/ConverterGUI.cs
@@ -9,40 +9,11 @@ namespace BrainfuckConverterGUI {
 
 		private void encodeButton_Click(object sender, EventArgs e) {
 			try {
-				string inputData = inputText.Text;
-
-                string inputCode = "";
                 Converter converter = new Converter();
 
-                foreach(char inputChar in inputData) {
-					if(inputChar >= 65 && inputChar <= 90)
-						inputCode += converter.Encode(inputChar, 6);
-					else if(inputChar >= 97 && inputChar <= 122)
-						inputCode += converter.Encode(inputChar, 7);
-					else if(inputChar >= 32 && inputChar <= 48)
-						inputCode += converter.Encode(inputChar, 8);
-					else if(inputChar >= 48 && inputChar <= 57)
-						inputCode += converter.Encode(inputChar, 5);
-					else if(inputChar >= 58 && inputChar <= 64)
-						inputCode += converter.Encode(inputChar, 4);
-					else if(inputChar >= 192 && inputChar <= 255)
-						inputCode += converter.Encode(inputChar, 9);
-					else if(inputChar >= 128 && inputChar <= 191)
-						inputCode += converter.Encode(inputChar, 3);
-					else if(inputChar >= 123 && inputChar <= 126)
-						inputCode += converter.Encode(inputChar, 2);
-					else if(inputChar >= 91 && inputChar <= 96)
-						inputCode += converter.Encode(inputChar, 1);
-					else if(inputChar < 0 || inputChar > 255)
-						continue;
-					else
-						inputCode += converter.Encode(inputChar, 0);
-				}
-
-				outputText.Show();
-				outputText.Text = converter.Setup + inputCode;
-
-			} catch(Exception error) {
+				outputText.Text = converter.Encode(inputText.Text);
+                outputText.Show();
+            } catch(Exception error) {
 				MessageBox.Show(error.Message);
 			}
 		}
