f6d3b6b1ba132fef903ce214a2181361e727d732
Hugo <54435874+Cactooz@users.noreply.github.com>
2023-01-24 10:58:47 +0100

    Change from Windows Forms to WPF GUI
    Complete rewrite of the program and GUI from Windows Forms to the newer and more supported WPF.

---
+++ BrainfuckInterceptor/ConverterGUI.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows;
+
+namespace BrainfuckInterceptor {
+	/// <summary>
+	/// Interaction logic for ConverterGUI.xaml
+	/// </summary>
+	public partial class ConverterGUI:Window {
+		public ConverterGUI() {
+			InitializeComponent();
+		}
+
+		private void EncodeButton_Click(object sender, RoutedEventArgs e) {
+			try {
+				Converter encoder = new Converter();
+				OutputText.Text = encoder.Encode(InputText.Text);
+				OutputLabel.Content = "BRAINFUCK Code Output";
+				OutputLabel.Visibility = Visibility.Visible;
+				OutputText.Visibility = Visibility.Visible;
+				CopyButton.Visibility = Visibility.Visible;
+			}
+			catch(Exception error) {
+				MessageBox.Show(error.Message);
+			}
+        }
+        private void DecodeButton_Click(object sender, RoutedEventArgs e) {
+            try {
+                Converter decoder = new Converter();
+                OutputText.Text = decoder.Decode(InputText.Text);
+                OutputLabel.Content = "Extended ASCII Text Output";
+                OutputLabel.Visibility = Visibility.Visible;
+                OutputText.Visibility = Visibility.Visible;
+                CopyButton.Visibility = Visibility.Visible;
+            }
+            catch(Exception error) {
+                MessageBox.Show(error.Message);
+            }
+        }
+
+        private void CopyButton_Click(object sender, RoutedEventArgs e) {
+			try {
+				Clipboard.SetText(OutputText.Text);
+			}
+			catch(Exception error) {
+				MessageBox.Show(error.Message);
+			}
+		}
+
+		private void InputText_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e) {
+			try {
+				if(InputText.Text.Length <= 0) {
+					EncodeButton.IsEnabled = false;
+					DecodeButton.IsEnabled = false;
+				}
+				else {
+					EncodeButton.IsEnabled = true;
+					DecodeButton.IsEnabled = true;
+				}
+
+				if(Regex.IsMatch(InputText.Text, @"[-+.<>[\]]"))
+					DecodeButton.IsEnabled = true;
+				else
+					DecodeButton.IsEnabled = false;
+			}
+			catch(Exception error) {
+				MessageBox.Show(error.Message);
+			}
+		}
+	}
+}
