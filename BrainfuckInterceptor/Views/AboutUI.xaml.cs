cd449803de9351435d93a4d66899e4ab5b55973f
Hugo <54435874+Cactooz@users.noreply.github.com>
2023-02-14 20:34:22 +0100

    Add About page with explanation and Github link

--- BrainfuckInterceptor/Views/AboutUI.xaml.cs
+++ BrainfuckInterceptor/Views/AboutUI.xaml.cs
@@ -1,28 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Diagnostics;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace BrainfuckInterceptor.Views
 {
-    /// <summary>
-    /// Interaction logic for AboutUI.xaml
-    /// </summary>
-    public partial class AboutUI : Page
-    {
-        public AboutUI()
-        {
-            InitializeComponent();
-        }
-    }
+	/// <summary>
+	/// Interaction logic for AboutUI.xaml
+	/// </summary>
+	public partial class AboutUI : Page
+	{
+		public AboutUI()
+		{
+			InitializeComponent();
+		}
+
+		private void GithubButton_Click(object sender, System.Windows.RoutedEventArgs e) {
+			Process.Start(new ProcessStartInfo {
+				FileName = "https://github.com/Cactooz/BrainfuckConverter",
+				UseShellExecute = true
+			});
+		}
+	}
 }
