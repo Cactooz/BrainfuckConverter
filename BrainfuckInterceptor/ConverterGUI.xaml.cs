8b49dcd629c62448ca9315e14bb2fd7c9e5650ef
Hugo <54435874+Cactooz@users.noreply.github.com>
2023-03-03 15:31:44 +0100

    Add custom window resizing

--- BrainfuckInterceptor/ConverterGUI.xaml.cs
+++ BrainfuckInterceptor/ConverterGUI.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Shapes;
 
 namespace BrainfuckInterceptor {
 	/// <summary>
@@ -8,50 +9,93 @@ namespace BrainfuckInterceptor {
 	/// </summary>
 	public partial class ConverterGUI:Window {
 
-		Uri converterUI = new Uri("Views/ConverterUI.xaml", UriKind.Relative);
-        Uri settingsUI = new Uri("Views/SettingsUI.xaml", UriKind.Relative);
-        Uri aboutUI = new Uri("Views/AboutUI.xaml", UriKind.Relative);
+		private Uri converterUI = new Uri("Views/ConverterUI.xaml", UriKind.Relative);
+		private Uri settingsUI = new Uri("Views/SettingsUI.xaml", UriKind.Relative);
+		private Uri aboutUI = new Uri("Views/AboutUI.xaml", UriKind.Relative);
 
 		public ConverterGUI() {
 			InitializeComponent();
-        }
+		}
 
-        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e) {
-            base.OnMouseLeftButtonDown(e);
-            DragMove();
-        }
+		/*protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e) {
+			base.OnMouseLeftButtonDown(e);
+			DragMove();
+		}*/
 
-        private void CloseButton_Click(object sender, RoutedEventArgs e) {
+		private void CloseButton_Click(object sender, RoutedEventArgs e) {
 			Close();
-        }
+		}
 
-        private void FullscreenButton_Click(object sender, RoutedEventArgs e) {
+		private void FullscreenButton_Click(object sender, RoutedEventArgs e) {
 			switch(WindowState) {
 				case WindowState.Normal:
-                    ResizeMode = ResizeMode.NoResize;
-                    WindowState = WindowState.Maximized;
-                    break;
-				case WindowState.Maximized:
-                    ResizeMode = ResizeMode.CanResizeWithGrip;
-                    WindowState = WindowState.Normal;
+					ResizeMode = ResizeMode.NoResize;
+					WindowState = WindowState.Maximized;
 					break;
-            }
-        }
+				case WindowState.Maximized:
+					ResizeMode = ResizeMode.CanResizeWithGrip;
+					WindowState = WindowState.Normal;
+					break;
+			}
+		}
 
-        private void MinimizeButton_Click(object sender, RoutedEventArgs e) {
-            WindowState = WindowState.Minimized;
-        }
+		private void MinimizeButton_Click(object sender, RoutedEventArgs e) {
+			WindowState = WindowState.Minimized;
+		}
 
-        private void ConverterButton_Click(object sender, RoutedEventArgs e) {
-            ViewFrame.Source = converterUI;
-        }
+		private void ConverterButton_Click(object sender, RoutedEventArgs e) {
+			ViewFrame.Source = converterUI;
+		}
 
-        private void SettingsButton_Click(object sender, RoutedEventArgs e) {
-            ViewFrame.Source = settingsUI;
-        }
+		private void SettingsButton_Click(object sender, RoutedEventArgs e) {
+			ViewFrame.Source = settingsUI;
+		}
 
-        private void AboutButton_Click(object sender, RoutedEventArgs e) {
-            ViewFrame.Source = aboutUI;
-        }
-    }
+		private void AboutButton_Click(object sender, RoutedEventArgs e) {
+			ViewFrame.Source = aboutUI;
+		}
+
+		private void ResizeMove(object sender, MouseEventArgs e) {
+			bool resizing = e.LeftButton == MouseButtonState.Pressed;
+			Rectangle senderRectangle = sender as Rectangle;
+
+			if(resizing) {
+				Window window = senderRectangle.Tag as Window;
+
+				if(senderRectangle != null) {
+					Point pos = e.GetPosition(window);
+					senderRectangle.CaptureMouse();
+
+					if(senderRectangle.Name.ToLower().Contains("right")) {
+						pos.X += 3;
+						if(pos.X > 0)
+							window.Width = pos.X;
+					} else if(senderRectangle.Name.ToLower().Contains("left")) {
+						pos.X -= 3;
+						if(pos.X < 0 || window.Width > window.MinWidth) {
+							window.Left += pos.X;
+							pos.X = window.Width - pos.X;
+							if(pos.X > 0)
+								window.Width = pos.X;
+						}
+					}
+
+					if(senderRectangle.Name.ToLower().Contains("bottom")) {
+						pos.Y += 3;
+						if(pos.Y > 0)
+							window.Height = pos.Y;
+					} else if(senderRectangle.Name.ToLower().Contains("top")) {
+						pos.Y -= 3;
+						if(pos.Y < 0 || window.Height > window.MinHeight) {
+							window.Top += pos.Y;
+							pos.Y = window.Height - pos.Y;
+							if(pos.Y > 0)
+								window.Height = pos.Y;
+						}
+					}
+				}
+			} else
+				senderRectangle.ReleaseMouseCapture();
+		}
+	}
 }
