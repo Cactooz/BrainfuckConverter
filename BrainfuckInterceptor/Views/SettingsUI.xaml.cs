2738947d157ebaac82c307779ad85878f93c62e6
Hugo <54435874+Cactooz@users.noreply.github.com>
2023-03-24 23:15:32 +0100

    Add Click Methods to save memory type in settings

--- BrainfuckInterceptor/Views/SettingsUI.xaml.cs
+++ BrainfuckInterceptor/Views/SettingsUI.xaml.cs
@@ -1,31 +1,40 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
 
-namespace BrainfuckInterceptor.Views
-{
-    /// <summary>
-    /// Interaction logic for SettingsUI.xaml
-    /// </summary>
-    public partial class SettingsUI : Page
-    {
-        public SettingsUI()
-        {
-            InitializeComponent();
-        }
+namespace BrainfuckInterceptor.Views {
+	/// <summary>
+	/// Interaction logic for SettingsUI.xaml
+	/// </summary>
+	public partial class SettingsUI:Page {
+		public SettingsUI() {
+			InitializeComponent();
+		}
 
-        private void ThemeButton_Click(object sender, RoutedEventArgs e) {
-            Skin currentSkin = App.Skin;
+		private void ThemeButton_Click(object sender, RoutedEventArgs e) {
+			Skin currentSkin = App.Skin;
 
-            switch(currentSkin) {
-                case Skin.Light:
-                ((App)Application.Current).ChangeSkin(Skin.Dark);
-                ThemeButton.Content = "Change to Light Theme";
-                break;
-                case Skin.Dark:
-                ((App)Application.Current).ChangeSkin(Skin.Light);
-                ThemeButton.Content = "Change to Dark Theme";
-                break;
-            }
-        }
-    }
+			switch(currentSkin) {
+				case Skin.Light:
+				((App)Application.Current).ChangeSkin(Skin.Dark);
+				ThemeButton.Content = "Change to Light Theme";
+				break;
+				case Skin.Dark:
+				((App)Application.Current).ChangeSkin(Skin.Light);
+				ThemeButton.Content = "Change to Dark Theme";
+				break;
+			}
+		}
+		private void MemoryASCIIButton_Click(object sender, RoutedEventArgs e) {
+			Properties.Settings.Default.memory = "ASCII";
+		}
+		private void MemoryExtendedASCIIButton_Click(object sender, RoutedEventArgs e) {
+			Properties.Settings.Default.memory = "ExtendedASCII";
+		}
+		private void MemoryLettersNumbersButton_Click(object sender, RoutedEventArgs e) {
+			Properties.Settings.Default.memory = "LettersNumbers";
+		}
+		private void MemoryEmptyButton_Click(object sender, RoutedEventArgs e) {
+			Properties.Settings.Default.memory = "Empty";
+		}
+	}
 }
