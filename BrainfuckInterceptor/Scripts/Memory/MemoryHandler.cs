a3ba5f5be616bbda90d31e18f1407ece711aac1a
Hugo <54435874+Cactooz@users.noreply.github.com>
2023-04-08 11:00:17 +0200

    Add Auto memory option

--- BrainfuckInterceptor/Scripts/Memory/MemoryHandler.cs
+++ BrainfuckInterceptor/Scripts/Memory/MemoryHandler.cs
@@ -6,6 +6,7 @@ namespace BrainfuckInterceptor.Scripts.Memory {
 		private readonly MemoryASCII ascii = new();
 		private readonly MemoryLettersNumbers lettersNumbers = new();
 		private readonly MemoryEmpty empty = new();
+		private readonly MemoryAuto auto = new();
 
 		public string Encode(string inputText) {
 			return Properties.Settings.Default.memory.ToLower() switch {
@@ -13,19 +14,9 @@ namespace BrainfuckInterceptor.Scripts.Memory {
 				"ascii" => ascii.Encode(inputText),
 				"lettersnumbers" => lettersNumbers.Encode(inputText),
 				"empty" => empty.Encode(inputText),
+				"auto" => auto.Encode(inputText),
 				_ => throw new NotSupportedException($"The set Memory type '{Properties.Settings.Default.memory}' is invalid. Choose a valid one in the settings panel.")
 			};
 		}
-
-		/* TODO:
-		 * Add the check from settings which memory should be used
-		 * Create that object with the correct class
-		 * 
-		 * Add Encode class that uses the correct memory
-		 * Should just have one "memory" object that is different dependent on the setting if possible,
-		 * in order to make just one encode method for everything
-		 * 
-		 * For the future, there will be an auto memory that uses the best possible for the input
-		 */
 	}
 }
